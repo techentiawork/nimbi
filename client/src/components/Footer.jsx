de9a0a5d4e91e18730cb3f5bf4f8d08dc1e88c2b
techentiawork <tweakify.help@gmail.com>
2024-09-26 00:03:14 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { discord, insta, telegram, xicon, yt } from '../assets'
+import reddit from "../assets/reddit.png"
 function Footer() {
     return (
         <>
@@ -7,11 +8,11 @@ function Footer() {
                     <div className="flex flex-col items-center gap-[7px]">
                         <p className='text-[#EBECED] '>Our community</p>
                         <ul className="list-none flex gap-[13px] items-center">
-                            <li><img src={discord} className='w-5 rounded-[4px] cursor-pointer' alt="" /></li>
-                            <li><img src={telegram} className='w-5 rounded-[4px] cursor-pointer' alt="" /></li>
-                            <li><img src={insta} className='w-5 rounded-[4px] cursor-pointer' alt="" /></li>
-                            <li><img src={xicon} className='w-5 rounded-[4px] cursor-pointer' alt="" /></li>
-                            <li><img src={yt} className='w-5 rounded-[4px] cursor-pointer' alt="" /></li>
+                            <li><img src={discord} className='w-5 rounded-[4px] cursor-pointer' alt="https://discord.gg/nimbi" /></li>
+                            <li><img src={telegram} className='w-5 rounded-[4px] cursor-pointer' alt="https://web.telegram.org/k/#@NimbiWolfpackVIP" /></li>
+                            <li><img src={telegram} className='w-5 rounded-[4px] cursor-pointer' alt="https://web.telegram.org/k/#@nimbitoken" /></li>
+                            <li><img src={reddit} className='w-5 rounded-[4px] cursor-pointer' alt="https://www.reddit.com/user/nimbitoken/?rdt=35960" /></li>
+                            <li><img src={yt} className='w-5 rounded-[4px] cursor-pointer' alt="https://www.youtube.com/@KaziSafari" /></li>
                         </ul>
                     </div>
                     <p className="text-white text-center text-[11px] font-normal font-roboto">Copyright &copy; 2024 NIMBI.COM | All rights reserved.</p>
